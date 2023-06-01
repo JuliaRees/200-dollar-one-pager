e7b9ce0e6a634298fd0b12fb47c2037e28100f8e
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-06-01 12:55:02 +0800

    jekyll

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $('.hamburger-button').click(function(){
+        $(this).toggleClass('active');
+        $(".mobile-menu").fadeToggle();
+      });
